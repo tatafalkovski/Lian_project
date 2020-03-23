20db3a7ccd114f7c2c3e70bc9b9d47e4150af626
Tata <tata@MacBook-Pro-nikita.local>
2020-03-23 21:03:53 +0200

    swipe_front-end

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("lol");
+var mySwiper = new Swiper(".swiper-container", {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: ".dots"
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".slider-button-right",
+        prevEl: ".slider-button-left"
+    }
+});
